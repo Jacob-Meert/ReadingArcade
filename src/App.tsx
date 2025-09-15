95c5f2824d17e3af361341959b2ed658f86a657c
Kasra Ghadimi <kasra.gh37@gmail.com>
2025-09-15 02:05:40 -0400

    merging

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import StackFireBall from "./games/StackFireBall.tsx";
+import TestPage from "./pages/Test";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/games/StackFireBall" element={<StackFireBall />} />
+          <Route path="/test" element={<TestPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
