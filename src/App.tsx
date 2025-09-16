3b15fdc1c590b9bead770e58e72588d01fdf5840
Kasra Ghadimi <kasra.gh37@gmail.com>
2025-09-15 22:28:28 -0400

    deleted stack fireball

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
-import StackFireBall from "./games/StackFireBall.tsx";
 import TestPage from "./games/Test.tsx";
 
 const queryClient = new QueryClient();
@@ -18,7 +17,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/games/StackFireBall" element={<StackFireBall />} />
           <Route path="/test" element={<TestPage />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
